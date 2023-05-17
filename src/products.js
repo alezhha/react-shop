4eb8de3359f77f2a60566c45bc0dd530994e543d
alezhha <88081396+alezhha@users.noreply.github.com>
2023-05-18 01:51:08 +0600

    yeap

---
+++ src/products.js
@@ -0,0 +1,24 @@
+import image1 from './assets/12429MADDt8.jpg';
+import image2 from './assets/ZYZnCpKHtPY.jpg';
+import image3 from './assets/xUAOuVann50.jpg';
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: 'Bebra1',
+    price: 990.0,
+    productImage: image1,
+  },
+  {
+    id: 2,
+    productName: 'Bebra2',
+    price: 990.0,
+    productImage: image2,
+  },
+  {
+    id: 3,
+    productName: 'Bebra3',
+    price: 990.0,
+    productImage: image3,
+  },
+]
